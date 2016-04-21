dbad33366397a6739c14f73fb34d9500a24a32f5
Joseph Mingrone <jrm@ftfl.ca>
2016-04-21 14:24:17 -0300

    Format the load average on gly
    Always print two digits to the right of the decimal for gly's load
    average.  This means the StumpWM mode-line text won't jump around as
    often.

--- ml_cpu.sh
+++ ml_cpu.sh
@@ -40,8 +40,8 @@ while kill -0 "$stump_pid" > /dev/null 2>&1; do
 		     hw.acpi.thermal.tz0.temperature | \
 		      tr '\n' ' ' | sed 's/.[0-9]C//g'
     elif [ "${hostname}" = "gly" ]; then
-	uptime | awk -F "load averages: " '{ print $2 }' | cut -d, -f1 \
-            | tr '\n' ' '
+	printf "%2.2f " $(uptime | awk -F "load averages: " '{ print $2 }' \
+                              | cut -d, -f1 | tr '\n' ' ')
 	/sbin/sysctl -n dev.cpu.0.temperature dev.cpu.1.temperature \
                      dev.cpu.2.temperature dev.cpu.3.temperature \
 	    | sed 's/.[0-9]C//' | paste -s -d ',' -
